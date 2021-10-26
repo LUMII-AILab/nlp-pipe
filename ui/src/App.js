698551bb0cd0ce3553ce87571df3f087c2b19b9d
Normunds Grūzītis <normundsg@gmail.com>
2021-10-26 21:19:48 +0300

    Minor fine-tuning of the demo UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -26,7 +26,7 @@ const Content = styled.div`
 `;
 
 const Footer = styled(Layout.Footer)`
-  font-size: 0.9em;
+  font-size: 1.0em;
 `;
 
 const App = () => (
@@ -39,16 +39,20 @@ const App = () => (
         <NlpView/>
       </Content>
     </ContentWrapper>
-    <Footer>
-      <p>This work is supported by the European Regional Development Fund under the grant agreement No. 1.1.1.1/16/A/219 (<a href="https://github.com/LUMII-AILab/FullStack">Full Stack of Language Resources for Natural Language Understanding and Generation in Latvian</a>) and by the State Research Programme under the grant agreement No. VPP-IZM-DH-2020/1-0001 (<a href="http://www.digitalhumanities.lv/projects/DHVPP-en/">Digital Resources for Humanities: Integration and Development</a>).</p>
-      <p>Source code available via <a href="https://github.com/LUMII-AILab/nlp-pipe">https://github.com/LUMII-AILab/nlp-pipe</a> </p>
+    <ContentWrapper style={{margin: "0px 20px 20px 20px", background: "#f0f2f5"}}>
+      <table style={{border: 0, padding: 0}}><tbody><tr><td>
+      <p>This work is supported by the European Regional Development Fund under the grant agreement No. 1.1.1.1/16/A/219 (<a href="https://github.com/LUMII-AILab/FullStack">Full Stack of Language Resources for Natural Language Understanding and Generation in Latvian</a>) and by the State Research Programme under the grant agreement No. VPP-IZM-DH-2020/1-0001 (<a href="http://www.digitalhumanities.lv/projects/DHVPP-en/">Digital Resources for Humanities: Integration and Development</a>) in synergy with <a href="https://clarin.lv/en-us/">CLARIN-LV</a>.</p>
+      <p>Source code of NLP-PIPE available via a public <a href="https://github.com/LUMII-AILab/nlp-pipe">GitHub repository</a>.</p>
       <p style={{marginBottom: 0}}>Publications:</p>
       <ul>
-        <li>A. Znotins and E. Cirule, NLP-PIPE: Latvian NLP Tool Pipeline, <i>Human Language Technologies -- The Baltic Perspective</i>, IOS Press, 2018, (<a href="http://ebooks.iospress.nl/volumearticle/50320">PDF</a>, <a href="http://dx.doi.org/10.3233/978-1-61499-912-6-183">DOI</a>, <a href="http://ailab.lv/publications/411/?bibtex">BibTeX</a>)</li>
-        <li>N. Gruzitis and A. Znotins , Multilayer Corpus and Toolchain for Full-Stack NLU in Latvian, <i>Proceedings of the CLARIN Annual Conference 2018</i>, 2018, (<a href="https://office.clarin.eu/v/CE-2018-1292-CLARIN2018_ConferenceProceedings.pdf">PDF</a>, <a href="http://ailab.lv/publications/413/?bibtex">BibTeX</a>)</li>
+        <li>Znotiņš, A. and Cīrule, E. NLP-PIPE: Latvian NLP Tool Pipeline. <i>Human Language Technologies - The Baltic Perspective</i>, IOS Press, 2018 (<a href="http://ebooks.iospress.nl/volumearticle/50320">PDF</a>, <a href="http://ailab.lv/publications/411/?bibtex">BibTeX</a>)</li>
+        <li>Grūzītis, N. and Znotiņš, A. Multilayer Corpus and Toolchain for Full-Stack NLU in Latvian. <i>Proceedings of the CLARIN Annual Conference</i>, 2018 (<a href="https://office.clarin.eu/v/CE-2018-1292-CLARIN2018_ConferenceProceedings.pdf">PDF</a>, <a href="http://ailab.lv/publications/413/?bibtex">BibTeX</a>)</li>
       </ul>
-      <img src="/projects.png" style={{width: '100%', maxWidth: 500}} alt="Projects" />
-    </Footer>
+      <p>&copy; <a href="http://ailab.lv/en/">Artificial Intelligence Laboratory</a> | Institute of Mathematics and Computer Science, University of Latvia</p>
+      </td><td style={{width: "20px"}}></td><td style={{background: "#fff"}}>
+      <img src="/projects.png" style={{minWidth: 500, maxWidth: 500}} alt="Projects" />
+      </td></tr></tbody></table>
+    </ContentWrapper>
   </CustomLayout>
 );
 
